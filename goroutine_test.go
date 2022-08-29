b4c5ebb5c6074c7f2e0380a0375b6168fb391ef3
Rahman Insani <rahmaninsani54@gmail.com>
2022-08-29 09:56:58 +0700

    Learn: Goroutine Basic

---
+++ goroutine_test.go
@@ -0,0 +1,18 @@
+package golang_goroutine
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func RunHelloWorld() {
+	fmt.Println("Hello World")
+}
+
+func TestCreateGoroutine(t *testing.T) {
+	go RunHelloWorld()
+	fmt.Println("Hi")
+
+	time.Sleep(1 * time.Second)
+}
