aa12012e9ebe8b2dfd75d38048e2ed75fdf562ad
Rahman Insani <rahmaninsani54@gmail.com>
2022-08-29 11:40:47 +0700

    Learn: Channel

---
+++ channel_test.go
@@ -0,0 +1,23 @@
+package golang_goroutine
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel <- "Bob"
+		fmt.Println("Selesai mengirim data ke channel")
+	}()
+
+	data := <-channel
+	fmt.Println(data)
+	time.Sleep(5 * time.Second)
+
+}
